0ba5f025de4ce29d4bb37207f7787348457caffc
furkangulluu <furkangulluu@gmail.com>
2022-01-16 19:04:54 +0300

    warning fix for deployment

--- src/components/LeftSide/Sorting/index.tsx
+++ src/components/LeftSide/Sorting/index.tsx
@@ -4,11 +4,9 @@ import { Card, Title, Container } from "./sorting.styled";
 import { useDispatch } from "react-redux";
 import { SortingType } from "../../../models/SortingType";
 import { GET_PRODUCT_BY_SORTING_SAGA } from "../../../redux/actions/sagaActions";
-import { useAppSelector } from "../../../hooks/slice-hook";
 
 const Sorting = () => {
   const dispatch = useDispatch();
-  let selectedSort = useAppSelector((state) => state.apiQuery);
 
   const change = (item: SortingType) => {
     dispatch({ type: GET_PRODUCT_BY_SORTING_SAGA, payload: item });
